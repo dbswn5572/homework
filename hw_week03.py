37be83c384724f08f8c4752f202b6dd7ee653c4d
dbswn5572 <39761506+dbswn5572@users.noreply.github.com>
2021-01-31 23:13:08 +0900

    source by genie chart

---
+++ hw_week03.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.geniedb
+
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713',headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+charts = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for chart in charts:
+    rank = chart.select_one('td.number').text[0:2].strip()
+    info = chart.select_one('td.info > a.title').text.strip()
+    artist = chart.select_one('td.info > a.artist').text.strip()
+    # print(rank, info, artist)
+    doc = {
+        'rank': rank,
+        'title': info,
+        'artist': artist
+    }
+    db.charts.insert_one(doc)
