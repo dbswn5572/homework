9f2a75ed7b82c629b9235be8a1328b667960db2b
dbswn5572 <39761506+dbswn5572@users.noreply.github.com>
2021-02-01 19:49:38 +0900

    Review 0201 - 4주차

--- hw_week03.py
+++ hw_week03.py
@@ -14,6 +14,15 @@ charts = soup.select('#body-content > div.newest-list > div > table > tbody > tr
 
 for chart in charts:
     rank = chart.select_one('td.number').text[0:2].strip()
+    ########################################
+    # Tip!
+    # 문자열도 0이상 2미만으로, 인덱스로 추출이 가능하다!
+    # a = "hello world"
+    # print(a[0:2]) > he
+    #
+    # OR
+    # .text.split()[0] 을 하면 배열의 형태로 가져와서 인덱스 0을 가져오게 할 수도 있어! === 이게 더 깔끔하게!
+    ########################################
     info = chart.select_one('td.info > a.title').text.strip()
     artist = chart.select_one('td.info > a.artist').text.strip()
     # print(rank, info, artist)
